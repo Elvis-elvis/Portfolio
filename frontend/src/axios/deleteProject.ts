c353b26698781e191e8504cdd5994053496b093b
s2232464 <2232464@champlaincollege.qc.ca>
2025-02-18 00:12:16 -0500

    Project

--- frontend/src/axios/deleteProject.ts
+++
@@ -1,6 +0,0 @@
-import axiosInstance from "@/Shared/Api/axiosInstance";
-
-export const deleteProject = async (projectId: string): Promise<void> => {
-    const backendUrl = process.env.REACT_APP_BACKEND_URL;
-    await axiosInstance.delete<void>(`${backendUrl}/api/project/${projectId}`);
-};
